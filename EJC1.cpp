373fa65ec57eed9aa2212828897976c42373579c
#SONY_FOX <131622618+brayanop@users.noreply.github.com>
2024-06-19 18:51:23 -0500

    Update EJC1.cpp

--- EJC1.cpp
+++ EJC1.cpp
@@ -1,1 +1,18 @@
-
+suma en un arreglo
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int A[100];
+	int N;
+	int suma = 0;
+	
+	cout<<"Ingrese N:"<<endl;
+	cin >> N;
+	cout<<"Ingrese numeros.."<<endl;
+	for(int i=0;i<N;i++){
+		cin >> A[i];
+		suma+= A[i]	;
+	}
+	cout<<"La suma es:"<< suma;
+	return 0;
+}
